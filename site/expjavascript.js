2aa19177206a86a89e6ad4a74742363c58bef5df
Roger Guillen <rogguicam@gmail.com>
2020-05-10 12:00:51 +0200

    setmana 5.2.1

--- site/expjavascript.js
+++ site/expjavascript.js
@@ -216,3 +216,25 @@ document.querySelector("body").addEventListener("mousemove",
     }
   }
   );
+document.addEventListener("DOMContentLoaded", 
+  function(event) {
+    document.querySelector("#testajax").addEventListener("click", function(){
+      $ajaxUtils.sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesLolis) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfT + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+    });

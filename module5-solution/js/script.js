3dffafb6b9fd2ccc5f2f8852d46bc82dc436a6a1
Roger Guillen <rogguicam@gmail.com>
2020-05-12 15:29:07 +0200

    setmana 5.3.9

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -104,8 +104,7 @@ $ajaxUtils.sendGetRequest(
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory (categories);
-
+      var 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -118,12 +117,10 @@ $ajaxUtils.sendGetRequest(
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = homeHtmlUrl
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
 
 // Given array of category objects, returns a random category object.
@@ -134,6 +131,7 @@ function chooseRandomCategory (categories) {
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
 }
+
 // Load the menu categories view
 dc.loadMenuCategories = function () {
   showLoading("#main-content");
